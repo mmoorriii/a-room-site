5fd222a3c507e8b08cb5c3f73c69f0f8d405ec13
mmorrii <byryshka55@gmail.com>
2023-03-09 10:25:52 +0200

    images are compressed

--- js/app.js
+++ js/app.js
@@ -4,11 +4,11 @@
 function isWebp() {
    // Проверка поддержки webp
    function testWebP(callback) {
-      let webP = new Image();
-      webP.onload = webP.onerror = function () {
-         callback(webP.height == 2);
+      let imgWebp = new Image();
+      imgWebp.onload = webP.onerror = function () {
+         callback(imgWebp.height == 2);
       };
-      webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+      imgWebp.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
    }
    // Добавление класса _webp или _no-webp для HTML
    testWebP(function (support) {
